b14ade251b850af59ca512be857c8c5b1c3745e4
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2020-09-18 17:06:56 +0300

    Clean 'lib' folder

---
+++ lib/facenet.py
@@ -0,0 +1,106 @@
+"""This file is imported from David Sandberg's 'facenet' repository (src folder)
+    https://github.com/davidsandberg/facenet/blob/master/src/facenet.py
+
+    The unnecessary parts for the purposes of this project were removed and the rest of the code was slightly cleaned.
+"""
+
+
+"""Functions for building the face recognition network.
+"""
+# MIT License
+# 
+# Copyright (c) 2016 David Sandberg
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import numpy as np
+import re
+
+
+def prewhiten(x):
+    mean = np.mean(x)
+    std = np.std(x)
+    std_adj = np.maximum(std, 1.0 / np.sqrt(x.size))
+    y = np.multiply(np.subtract(x, mean), 1 / std_adj)
+
+    return y
+
+
+def crop(image, random_crop, image_size):
+    if image.shape[1] > image_size:
+        sz1 = int(image.shape[1] // 2)
+        sz2 = int(image_size // 2)
+        if random_crop:
+            diff = sz1-sz2
+            (h, v) = (np.random.randint(-diff, diff + 1), np.random.randint(-diff, diff + 1))
+        else:
+            (h, v) = (0, 0)
+        image = image[(sz1 - sz2 + v):(sz1 + sz2 + v), (sz1 - sz2 + h):(sz1 + sz2 + h), :]
+
+    return image
+
+
+def flip(image, random_flip):
+    if random_flip and np.random.choice([True, False]):
+        image = np.fliplr(image)
+
+    return image
+
+
+def to_rgb(img):
+    w, h = img.shape
+    ret = np.empty((w, h, 3), dtype=np.uint8)
+    ret[:, :, 0] = ret[:, :, 1] = ret[:, :, 2] = img
+
+    return ret
+
+
+def load_image(img, do_random_crop, do_random_flip, image_size, do_prewhiten=True):
+    image = np.zeros((1, image_size, image_size, 3))
+    if img.ndim == 2:
+            img = to_rgb(img)
+    if do_prewhiten:
+            img = prewhiten(img)
+    img = crop(img, do_random_crop, image_size)
+    img = flip(img, do_random_flip)
+    image[:, :, :, :] = img
+
+    return image
+
+
+def get_model_filenames(model_dir):
+    files = os.listdir(model_dir)
+    meta_files = [s for s in files if s.endswith('.meta')]
+    if len(meta_files)==0:
+        raise ValueError('No meta file found in the model directory (%s)' % model_dir)
+    elif len(meta_files)>1:
+        raise ValueError('There should not be more than one meta file in the model directory (%s)' % model_dir)
+    meta_file = meta_files[0]
+    meta_files = [s for s in files if '.ckpt' in s]
+    max_step = -1
+    for f in files:
+        step_str = re.match(r'(^model-[\w\- ]+.ckpt-(\d+))', f)
+        if step_str is not None and len(step_str.groups())>=2:
+            step = int(step_str.groups()[1])
+            if step > max_step:
+                max_step = step
+                ckpt_file = step_str.groups()[0]
+
+    return meta_file, ckpt_file
